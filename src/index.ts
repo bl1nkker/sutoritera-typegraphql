ccc7c63ad29b9c1c846b74021a9b9503ab4ed16d
Auezkhan Daniyar <nurovich14@gmail.com>
2021-08-12 18:46:22 +0600

    init socketio

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,14 @@ import { resolvers } from './resolvers'
 import { typeDefs } from './typeDefs'
 import { MyContext } from './types/MyContext'
 import { authMiddleware } from './middleware/authMiddleware'
+import { Server, Socket } from 'socket.io'
+import { createServer } from 'http'
+import cors from 'cors'
 
 const main = async() =>{
     const app:any = express()
     app.use(authMiddleware)
+    app.use(cors())
     
     const apolloServer = new ApolloServer({
         typeDefs,
@@ -20,7 +24,16 @@ const main = async() =>{
     apolloServer.applyMiddleware({ app })
     await mongoose.connect('mongodb://localhost:27017/sutoritera-dev', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify:false});
 
-    app.listen(4000, () => console.log('Server started on http://localhost:4000/graphql'))
+    const server = createServer(app)
+    const io = new Server(server);
+
+    io.on('connection', (socket:Socket) => {
+        console.log('a user connected');
+        socket.on('disconnect', () => {
+            console.log('a user disconnected')
+        })
+      });
+    server.listen(4000, () => console.log('Server started on http://localhost:4000/graphql'))
 }
 
 main()
